78f794ad8fdd7b8e16ca843fa574be9694a8cb7a
Alejandro Alberoni <atralberoni@inf.ufpel.edu.br>
2024-11-03 02:47:29 -0300

    first landing design, starting project

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,38 @@
-import Image from "next/image";
+import Spline from '@splinetool/react-spline/next';
+import { Space_Grotesk } from 'next/font/google'
+import { Righteous } from 'next/font/google';
 
+//const splineScene = "https://prod.spline.design/p4iP4wTGWNRNkBXe/scene.splinecode"
+//const splineScene = "https://prod.spline.design/ZA7tlSHuDNKipbn8/scene.splinecode"
+const splineScene = "https://prod.spline.design/oLwbthR5P4hZa54G/scene.splinecode"
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['700'],
+})
+const righteous = Righteous({
+  subsets: ['latin'],
+  weight: ['400'],
+})
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <Spline className='absolute place-self-center w-auto' scene={splineScene} />
+      <div className='flex flex-col select-none'>
+        <div className={`${righteous.className} absolute mt-4 w-full text-xl text-center text-white text-nowrap z-[-1]`}>
+          A. T. R. A.
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={`${spaceGrotesk.className} text-center text-white text-[36px] lg:text-[48px] mt-[50%] lg:mt-20 mx-10 lg:mx-0 antialiased z-10 pointer-events-none`}>
+          <p>Welcome to my personal and portfolio website.</p>
+          <p>I’m a computer science bachelor located in Brazil.</p>
+        </div>
+        <div className={`${spaceGrotesk.className} mt-[50vh] lg:mt-[20%] text-[36px] lg:text-[48px] text-white mx-10 z-10 pointer-events-none`}>
+          <p className='text-center lg:text-justify'>
+            &quot;This project blends creativity with purpose, showcasing my skills in web design, front-end, and back-end development while serving as a practical tool in my daily life.&ldquo;
+          </p>
+          <p className='float-end lg:mr-40 mt-10 lg:mt-0 '>-ChatGPT</p>
+        </div>
+      </div>
     </div>
   );
 }
